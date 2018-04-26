06f2fc88733baa46ef8854b1fc76190a835f46f0
Colloque Tsui <colloquet@gmail.com>
2018-04-26 17:11:50 +0800

    optimizations

--- src/components/ModePicker.js
+++ src/components/ModePicker.js
@@ -3,8 +3,11 @@ import styled from 'styled-components'
 
 import Button from './Button'
 import Muted from './Muted'
+
 import mode1 from '../assets/images/mode-1.jpg'
+import mode1Thumbnail from '../assets/images/mode-1-thumbnail.jpg'
 import mode2 from '../assets/images/mode-2.jpg'
+import mode2Thumbnail from '../assets/images/mode-2-thumbnail.jpg'
 
 const Container = styled.div`
   position: fixed;
@@ -31,14 +34,14 @@ function ModePicker({ onPick, stats }) {
       <p>請選擇模式</p>
 
       <Button onClick={() => onPick(1)} style={{ padding: '0.5rem' }}>
-        <Image src={mode1} alt="Mode 1" />
+        <Image src={mode1Thumbnail} alt="Mode 1" />
       </Button>
       <a href={mode1} target="_blank">
         <Muted>原圖</Muted>
       </a>
 
       <Button onClick={() => onPick(2)} style={{ marginTop: '2rem', padding: '0.5rem' }}>
-        <Image src={mode2} alt="Mode 2" />
+        <Image src={mode2Thumbnail} alt="Mode 2" />
       </Button>
       <a href={mode2} target="_blank">
         <Muted>原圖</Muted>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import NotificationProvider from './NotificationProvider'
 import App from './App'
-import './index.css'
 
 ReactDOM.render(
   <NotificationProvider>
